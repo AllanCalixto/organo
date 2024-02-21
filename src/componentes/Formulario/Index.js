4900932248e872f23b2c77ec2806074f8b13c568
Allan Calixto <allancalixto1@gmail.com>
2024-02-21 18:19:08 -0300

    feature: css do botão criar form

--- src/componentes/Formulario/Index.js
+++ src/componentes/Formulario/Index.js
@@ -1,3 +1,4 @@
+import Botao from '../Botao/Index'
 import CampoTexto from '../CampoTexto/CampoText'
 import ListaSuspensa from '../ListaSuspensa/Index'
 import './Formulario.css'
@@ -11,17 +12,24 @@ const Formulario = () => {
         'DevOps',
         'UX e Desing',
         'Inovação e Gestão'
-
     ]
+
+    const AoSalvar = (event) => {
+        event.preventDefault()
+        console.log("Formulário foi submetido")
+    }
 
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={ AoSalvar }>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome" />
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+                <CampoTexto obrigatorio={ true } label="Nome" placeholder="Digite seu nome" />
+                <CampoTexto obrigatorio={ true } label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-                <ListaSuspensa label="Time" itens={times}/>
+                <ListaSuspensa obrigatorio={ true } label="Time" itens={times}/>
+                <Botao>
+                    Criar Card
+                </Botao>    
             </form>
         </section>
 
